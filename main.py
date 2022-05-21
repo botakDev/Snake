83c9bbb858c2fc429c79ea22925ed746d995e200
Robert Laskiewicz <botak192.128@gmail.com>
2022-05-21 16:28:39 +0200

    added score, fixed code

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ import pygame
 from pygame import Vector2
 import random
 
-def check_same_pos(rect, rects):
+def same_pos(rect, rects):
     for i in range(len(rects)):
         if rect.center == rects[i].center:
-            return True
-    return False
-
+            return False
+    return True
 
 #game initialize
 pygame.init()
-clock = pygame.time.Clock()
-timer = 0
+
 
 #window
 pygame.display.set_caption("Snake")
 window_width = 1600
 window_height = 900
 window = pygame.display.set_mode((window_width, window_height))
+
+#settings
+clock = pygame.time.Clock()
+timer = 0
+box_width = 20
+box_height = 20
+rows = int(window_width / box_width)
+columns = int(window_height / box_height)
+font = pygame.font.Font("pixel_font.ttf", 50)
 
 #colors
 BLACK = pygame.Color(0, 0, 0)
@@ -28,27 +35,45 @@ RED = pygame.Color(255, 15, 15)
 LIGHT_GRAY = pygame.Color(79, 79, 79)
 DARK_GRAY = pygame.Color(43, 43, 43)
 
+def new_snake_part(snake_width, snake_height):
+    body_pos = Vector2(snake_parts[snake_len - 1].centerx,
+                        snake_parts[snake_len - 1].centery)
+    body = pygame.rect.Rect(-100, -100, snake_width, snake_height)
+    body.center = body_pos
+    return body
+
 #snake
-snake_width = 45
-snake_height = 45
-space = 5
+snake_width = 16
+snake_height = 16
 snake_g_colors = [255]
 
 direction = "RIGHT"
-changed_direction = False
 
 snake_parts = []
-snake_head = pygame.rect.Rect(252, 252, snake_width, snake_height)
+snake_head_pos = Vector2(int(rows / 2) * box_width, int(columns / 2) * box_height)
+snake_head = pygame.rect.Rect(0, 0, snake_width, snake_height)
+snake_head.center = snake_head_pos
 snake_parts.append(snake_head)
 
+def new_apple(apple_width, apple_height):
+    apple_pos = Vector2(random.randint(1, int(window_width / box_width) - 1) * box_width,
+                        random.randint(1, int(window_height / box_height) - 1) * box_height)
+    apple = pygame.rect.Rect(-100, -100, apple_width, apple_height)
+    apple.center = apple_pos
+    return apple
+
 #apples
-apple_width = 35
-apple_height = 35
+apple_width = 12
+apple_height = 12
 
 apples = []
-apple = pygame.rect.Rect(random.randint(1, 9) * 50 + 7, random.randint(1, 9) * 50 + 7, apple_width,
-                         apple_height)
+apple = new_apple(apple_width, apple_height)
 apples.append(apple)
+
+#score
+score = 0
+
+score_text = font.render(f"score: {score}", True, WHITE)
 
 #game loop
 run = True
@@ -61,66 +86,64 @@ while run:
             run = False
             pygame.quit()
 
-        if event.type == pygame.KEYDOWN:
-            if changed_direction == False:
-                if pygame.key.get_pressed()[pygame.K_UP] and direction != "DOWN":
-                    direction = "UP"
-                elif pygame.key.get_pressed()[pygame.K_DOWN] and direction != "UP":
-                    direction = "DOWN"
-                elif pygame.key.get_pressed()[pygame.K_LEFT] and direction != "RIGHT":
-                    direction = "LEFT"
-                elif pygame.key.get_pressed()[pygame.K_RIGHT] and direction != "LEFT":
-                    direction = "RIGHT"
-                changed_direction = True
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP] and direction != "DOWN":
+            direction = "UP"
+        elif pressed[pygame.K_DOWN] and direction != "UP":
+            direction = "DOWN"
+        elif pressed[pygame.K_LEFT] and direction != "RIGHT":
+            direction = "LEFT"
+        elif pressed[pygame.K_RIGHT] and direction != "LEFT":
+            direction = "RIGHT"
 
     if timer >= 40:
-        changed_direction = False
 
         snake_len = len(snake_parts)
-        apple_len = len(apples)
 
         for i in range(snake_len):
             if snake_len - i - 2 >= 0:
-                snake_parts[snake_len - i - 1].center = snake_parts[snake_len - i - 2].center
-
+                new_pos = snake_parts[snake_len - i - 2].center
+                snake_parts[snake_len - i - 1].center = new_pos
         if direction == "UP":
-            snake_parts[0].center += Vector2(0, -50)
+            snake_parts[0].center += Vector2(0, -box_height)
         elif direction == "DOWN":
-            snake_parts[0].center += Vector2(0, 50)
+            snake_parts[0].center += Vector2(0, box_height)
         elif direction == "LEFT":
-            snake_parts[0].center += Vector2(-50, 0)
+            snake_parts[0].center += Vector2(-box_width, 0)
         elif direction == "RIGHT":
-            snake_parts[0].center += Vector2(50, 0)
+            snake_parts[0].center += Vector2(box_width, 0)
 
+        snake_head = snake_parts[0]
         for i in range(snake_len):
-            if i >= 1:
-                if snake_parts[0].center == snake_parts[i].center:
+            if i != 0:
+                if snake_head.center == snake_parts[i].center:
                     run = False
                     pygame.quit()
-            if snake_parts[0].centerx < 0 or snake_parts[0].centerx > window_width or \
-                        snake_parts[0].centery < 0 or snake_parts[0].centery > window_height:
-                run = False
-                pygame.quit()
+        if snake_head.centerx <= 0 or snake_head.centerx >= window_width or \
+            snake_head.centery <= 0 or snake_head.centery >= window_height:
+            run = False
+            pygame.quit()
 
-        for i in range(apple_len):
-            if apples[i].center == snake_parts[0].center:
-                apples.pop(i)
-                apple = pygame.rect.Rect(random.randint(1, int(window_width / (snake_width + 5)) - 1) * (snake_height + 5) + 7,
-                                         random.randint(1, int(window_height / (snake_height + 5)) - 1) * (snake_height + 5) + 7,
-                                         apple_width, apple_height)
-                while check_same_pos(apple, snake_parts):
-                    apple = pygame.rect.Rect(
-                        random.randint(1, int(window_height / (snake_width + 5)) - 1) * (snake_height + 5) + 7,
-                        random.randint(1, int(window_height / (snake_height + 5)) - 1) * (snake_height + 5) + 7,
-                        apple_width, apple_height)
+        if apples[0].center == snake_parts[0].center:
+            apples.pop(0)
 
-                apples.append(apple)
+            apple = new_apple(apple_width, apple_height)
+            while not same_pos(apple, snake_parts):
+                apple = new_apple(apple_width, apple_height)
+            apples.append(apple)
 
-                snake_body = pygame.rect.Rect(snake_parts[snake_len - 1].x, snake_parts[snake_len - 1].y, snake_width, snake_height)
-                snake_parts.append(snake_body)
-                snake_g_colors.append(snake_g_colors[len(snake_g_colors) - 1] - 2.3)
+            snake_part = new_snake_part(snake_width, snake_height)
+            snake_parts.append(snake_part)
+
+            score += 1
+
+            last_color = snake_g_colors[len(snake_g_colors) - 1]
+            snake_g_colors.append(last_color - 0.5)
+
+        score_text = font.render(f"score: {score}", True, WHITE)
 
         timer -= 40
+
 
     window.fill(BLACK)
 
@@ -130,5 +153,7 @@ while run:
     for i in range(len(snake_parts)):
         color = pygame.Color(5, int(snake_g_colors[i]), 5)
         pygame.draw.rect(window, color, snake_parts[i])
+
+    window.blit(score_text, (10, 10))
 
     pygame.display.update()
